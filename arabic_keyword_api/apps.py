78953a7dbccf69b2019106f9d438bdf11e8c5426
3bdalla3adil <abdullagozer@gmail.com>
2022-06-06 23:10:07 +0200

    changed appconfig name in apps.py

--- arabic_keyword_api/apps.py
+++ arabic_keyword_api/apps.py
@@ -2,5 +2,7 @@ from django.apps import AppConfig
 
 
 class ArabicKeywordApiConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
     name = 'arabic_keyword_api'
+
+class TodosConfig(AppConfig):
+    name = 'todos'
