11793ad368a08ddafa9897c4c492a3e335d05826
3bdalla3adil <abdullagozer@gmail.com>
2022-06-26 16:01:15 +0200

    arabic translations list as string and simpilfied

--- arabic_keyword_api/tests.py
+++ arabic_keyword_api/tests.py
@@ -1,3 +1,18 @@
+from ast import Import
+from http.client import ImproperConnectionState
 from django.test import TestCase
+from . models    import keyword
 
-# Create your tests here.
+# import unittest
+
+class TestStringUtils(TestCase):
+
+    def test_Reverse(self):
+        self.assertEqual(keyword.objects.create("Hello, world"      ),"dlrow ,olleH"      )
+        self.assertEqual(keyword.objects.create("Crowdbotics"       ), "scitobdworC"      )
+        self.assertEqual(keyword.objects.create("Hello, 世界"       ), "界世 ,olleH"       )
+        self.assertEqual(keyword.objects.create("نص عربي"          ), "يبرع صن"          )
+        self.assertEqual(keyword.objects.create("نَصٌ عَربِيٌّ"          ),"ٌّيِبرَع ٌصَن"           )
+        self.assertEqual(keyword.objects.create("نَصٌ عَربِيٌّ!"         ), "!ٌّيِبرَع ٌصَن"         )
+        self.assertEqual(keyword.objects.create("نَصٌ example, عَربِيٌّ!"), "!ٌّيِبرَع ,elpmaxe ٌصَن")
+        self.assertEqual(keyword.objects.create(""                  ), ""                 )
