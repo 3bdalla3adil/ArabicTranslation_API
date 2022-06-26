a66cec97d3d4f3f1874ee964b01990a07a17e137
3bdalla3adil <abdullagozer@gmail.com>
2022-06-26 06:59:30 +0200

    support python command for linux,macos and windows

--- arabic_keyword_api/models.py
+++ arabic_keyword_api/models.py
@@ -1,5 +1,4 @@
  
-import subprocess
 from django.db    import models
     
 class keyword(models.Model):
@@ -8,10 +7,15 @@ class keyword(models.Model):
     keyword_translations = models.CharField("Ttranslated Keyword", blank=True,default='لاشيء',max_length=200)
 
     def get_translation(self):
-        output = str(subprocess.check_output(['py' ,'babla',self.keyword_text], shell=True).decode('utf-8'))
+        import platform,subprocess
+        if platform.system() == 'Linux' or 'Darwin' :
+            output = str(subprocess.check_output(['python3' ,'babla',self.keyword_text], shell=True).decode('utf-8'))
+          
+        elif platform.system() == 'Windows':
+            output = str(subprocess.check_output(['py' ,'babla',self.keyword_text], shell=True).decode('utf-8'))
         
         self.keyword_translations = str({self.keyword_text:output})
-        print('output='+output)
+        # print('output='+output)
         return {self.keyword_text:output}
         
 
